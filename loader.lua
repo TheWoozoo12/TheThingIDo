2ae5752aae13822a8c181e55b6b41dd8d8013878
The_Woozoo <69330974+TheWoozoo12@users.noreply.github.com>
2022-04-30 15:06:50 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,17 +1,17 @@
 if game.ReplicatedStorage:FindFirstChild("ACS_Engine") then
 	if game.ReplicatedStorage.ACS_Engine:FindFirstChild("Eventos") then
 		if game.ReplicatedStorage.ACS_Engine.Eventos:FindFirstChild("AccessId") then
-			loadstring(game:HttpGet(("https://raw.githubusercontent.com/TheWoozoo12/TheThingIDo/main/1.7.7.lua"), true))()
 			print("1.7.7")
+			loadstring(game:HttpGet(("https://raw.githubusercontent.com/TheWoozoo12/TheThingIDo/main/1.7.7.lua"), true))()
 		else
-			loadstring(game:HttpGet(("https://raw.githubusercontent.com/TheWoozoo12/TheThingIDo/main/1.5.lua"), true))()
 			print("1.5-1.7.6")
+			loadstring(game:HttpGet(("https://raw.githubusercontent.com/TheWoozoo12/TheThingIDo/main/1.5.lua"), true))()
 		end
 	else
 		local config = require(game.ReplicatedStorage.ACS_Engine.GameRules.Config)
 		if config.Version == "ACS 2.0.1 - R6" or config.Version == "ACS 2.0.0 - R15" then
-			loadstring(game:HttpGet(("https://raw.githubusercontent.com/TheWoozoo12/TheThingIDo/main/2.0.0.lua"), true))()
 			print("2.0.0-2.0.1")
+			loadstring(game:HttpGet(("https://raw.githubusercontent.com/TheWoozoo12/TheThingIDo/main/2.0.0.lua"), true))()
 		end
 	end
 else 
